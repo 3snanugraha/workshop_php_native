8c49a845b39aac7265e61d6363051a5adbf8a800
Tris <trisnanugraha87@gmail.com>
2024-11-18 08:36:41 +0700

    "Disable error reporting and display in controllers"

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,8 +1,8 @@
 <?php
-// dev_mode = 1
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// dev_mode = 0
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 session_start();
 require 'function.php';
